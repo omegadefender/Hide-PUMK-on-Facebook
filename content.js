0f62b01a97f2d8c1136ac312a061e19a680028d5
James Hastings <hastings.james.m@gmail.com>
2021-07-29 22:46:42 +1200

    deleted right div

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-let pumkdiv = document.evaluate("//*[text() = 'People you may know']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+let pumkdiv = document.evaluate("//span[text() = 'People you may know']/../../../../../div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
 console.log(pumkdiv)
 pumkdiv.singleNodeValue.remove();
 console.log("The 'Hide People You May Know' Chrome Extension for Facebook is Awesome");
