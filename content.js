480f2d3a1b3ed317536f57c285b209ee093b9fcd
James Hastings <hastings.james.m@gmail.com>
2021-08-06 12:14:57 +1200

    refactor and found button elements

--- content.js
+++ content.js
@@ -1,4 +1,14 @@
-let pumkdiv = document.evaluate("//span[text() = 'People you may know']/../../../../../div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-console.log(pumkdiv)
-pumkdiv.singleNodeValue.remove();
-console.log("The 'Hide People You May Know' Chrome Extension for Facebook is Awesome");
+function hidePUMK() {
+  let pumkdiv = document.evaluate("//span[text() = 'People you may know']/../../../../../div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+  if (pumkdiv.singleNodeValue == null) {
+    console.log("People you may know didn't spawn on this page")
+  } 
+  else {
+    pumkdiv.singleNodeValue.remove();
+    console.log("People you may know has been hidden")    
+  }
+}
+
+hidePUMK();
+const homebtn = document.querySelector('[aria-label="Home"]');
+const profilePage = document.querySelector("a[href='/me/']")
