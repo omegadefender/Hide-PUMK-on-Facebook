d6f6ddcdd53c5e298d42d4db831e423fba047c98
James Hastings <hastings.james.m@gmail.com>
2021-08-12 21:26:13 +1200

    refactored

--- content.js
+++ content.js
@@ -1,11 +1,10 @@
 const node = document.body;
-const config = { childList: true, subtree: true, characterData: true };
+const config = { childList: true, subtree: true };
 
 let observer = new MutationObserver(function(mutations, observer) {
     mutations.forEach(function(mutation) {
      let url = window.location.href;
      if (mutation.addedNodes.length && url != 'https://www.facebook.com/friends') {
-        console.log(url);
         hidePUMK();
     }
   })
@@ -15,11 +14,7 @@ observer.observe(node, config);
 
 function hidePUMK() {
   let pumkdiv = document.evaluate("//span[text() = 'People you may know']/../../../../../div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-  if (pumkdiv.singleNodeValue == null) {
-    console.log("People you may know didn't spawn on this page")
+  if (pumkdiv.singleNodeValue != null) {
+      pumkdiv.singleNodeValue.remove();
   } 
-  else {
-    pumkdiv.singleNodeValue.remove();
-    console.log("People you may know has been hidden")   
-  }
 }
