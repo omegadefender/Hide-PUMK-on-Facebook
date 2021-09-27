8037afc08c7b82daa4de01e3d116e39572d7e148
James Hastings <hastings.james.m@gmail.com>
2021-09-27 21:03:43 +1300

    added option ui

--- content.js
+++ content.js
@@ -6,6 +6,7 @@ let observer = new MutationObserver(function(mutations, observer) {
      let url = window.location.href;
      if (mutation.addedNodes.length && url != 'https://www.facebook.com/friends') {
         hidePUMK();
+        hideSuggestedGroups();
     }
   })
 });
@@ -14,10 +15,19 @@ observer.observe(node, config);
 
 function hidePUMK() {
   let pumkdiv = document.querySelector('[aria-label="People you may know"]');
-  let xpath = "//span[text() = 'People you may know']/../../../../../../../.."
+  let xpath = "//span[text() = 'People you may know']/../../../../../../../..";
   let pumkTextDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
   if (pumkdiv != null) {
       pumkdiv.remove();
       pumkTextDiv.remove();
-  } 
+  }
+}
+
+function hideSuggestedGroups() {
+  let xpath = "//span[text() = 'Suggested for you']/../../../../../../../../../../..";
+  let sugGroupsDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  if (sugGroupsDiv != null) {
+    sugGroupsDiv.remove();
+  }  
 }
