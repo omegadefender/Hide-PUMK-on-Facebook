8345d42bc088908a0069576e4dfea8b50a507467
James Hastings <hastings.james.m@gmail.com>
2021-10-01 12:22:58 +1300

    options working, not used yet in content

---
+++ options.js
@@ -0,0 +1,34 @@
+const saveButton = document.getElementById('save')
+
+function save_options() {
+    const pumkNews = document.getElementById('pumk1').checked;
+    const pumkProfile = document.getElementById('pumk2').checked;
+    const sugGroups = document.getElementById('sugGroups').checked;
+    chrome.storage.sync.set({
+      pumk1: pumkNews,
+      pumk2: pumkProfile,
+      sugGroups: sugGroups
+    }, function() {
+      // Update status to let user know options were saved.
+      const status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+  }
+  
+function restore_options() {
+  chrome.storage.sync.get({
+    pumk1: true,
+    pumk2: true,
+    sugGroups: true
+}, function(items) {
+    document.getElementById('pumk1').checked = items.pumk1;
+    document.getElementById('pumk2').checked = items.pumk2;
+    document.getElementById('sugGroups').checked = items.sugGroups
+  });
+}
+  
+document.addEventListener('DOMContentLoaded', restore_options);
+saveButton.addEventListener('click', save_options);
