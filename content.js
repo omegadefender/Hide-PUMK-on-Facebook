b2d044478a38100f9efd4b61988d82c06f0a4acb
James Hastings <hastings.james.m@gmail.com>
2021-10-05 23:54:01 +1300

    options working and refactored

--- content.js
+++ content.js
@@ -1,13 +1,17 @@
-const node = document.body;
-const config = { childList: true, subtree: true };
+const node = document.body
+const config = { childList: true, subtree: true }
 
 let observer = new MutationObserver(function(mutations, observer) {
-    mutations.forEach(function(mutation) {
-     let url = window.location.href;
-     if (mutation.addedNodes.length && url != 'https://www.facebook.com/friends') {
-        checkOptions('pumk2', hidePUMK)
-        //hidePUMK();
-        hideSuggestedGroups();
+  mutations.forEach(function(mutation) {
+    const url = window.location.href;
+    const urlIdex = url.indexOf(".")
+    const change = mutation.addedNodes.length
+    if (change && url == 'https://www.facebook.com/') {
+      checkOptions('pumk1', hidePUMK)
+      hideSuggestedGroups()
+    }
+    if (change && urlIdex != -1) {
+      checkOptions('pumk2', hidePUMK)
     }
   })
 });
@@ -20,23 +24,20 @@ function checkOptions(key, callback) {
   })
 }
 
-function hidePUMK(oVal) {
-  const optVal = oVal
-  let pumkdiv = document.querySelector('[aria-label="People you may know"]');
-  let xpath = "//span[text() = 'People you may know']/../../../../../../../..";
-  let pumkTextDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-  if (pumkdiv != null && optVal) {
-      pumkdiv.remove();
-      pumkTextDiv.remove();
+function hidePUMK(value) {
+  const xpath = "//span[text() = 'People you may know']/../../../../../../../.."
+  const pumkDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+  if (pumkDiv != null && value) {    
+    pumkDiv.remove()
+    console.log("Pumk removed")
   }
 }
 
 function hideSuggestedGroups() {
-  let xpath = "//span[text() = 'Suggested for you']/../../../../../../../../../../..";
-  let sugGroupsDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  
+  let xpath = "//span[text() = 'Suggested for you']/../../../../../../../../../../.."
+  let sugGroupsDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
   if (sugGroupsDiv != null) {
-    sugGroupsDiv.remove();
+    sugGroupsDiv.remove()
+    console.log("sugGroups Removed")
   }  
 }
