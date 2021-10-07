64b584865dc668ea9dd0d10551c45a50b3ca159a
James Hastings <hastings.james.m@gmail.com>
2021-10-07 23:01:09 +1300

    added stories and rooms

--- content.js
+++ content.js
@@ -7,18 +7,20 @@ let observer = new MutationObserver(function(mutations, observer) {
     const url = urlChopper(window.location.href)
     const urlIdex = url.indexOf(".")   
     if (change && url == '') {
-      checkOptions('pumk1', hidePUMK)
-      checkOptions('sugGroups', hideSuggestedGroups)
+      checkOption('pumk1', pumk)
+      checkOption('sugGroups', suggestedForYou)
+      checkOption('stories', stories)
+      checkOption('rooms', rooms)
     }
     if (change && urlIdex != -1) {
-      checkOptions('pumk2', hidePUMK)
+      checkOption('pumk2', hidePUMK)
     }
   })
 });
 
 observer.observe(node, config);
 
-function checkOptions(key, callback) {
+function checkOption(key, callback) {
   chrome.storage.sync.get(key, function(options) {
     callback(options[key])
   })
@@ -30,7 +32,7 @@ function urlChopper(url) {
   return newurl
 }
 
-function hidePUMK(value) {
+function pumk(value) {
   const xpath = "//span[text() = 'People you may know']/../../../../../../../.."
   const pumkDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
   if (pumkDiv != null && value) {    
@@ -38,11 +40,26 @@ function hidePUMK(value) {
   }
 }
 
-function hideSuggestedGroups(value) {
-  const xpath = "//span[text() = 'Suggested for you']/../../../../../../../../../../.."
-  const sugGroupsDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
-  if (sugGroupsDiv != null && value) {
-    sugGroupsDiv.remove()
-    console.log("sugGroups Removed")
+function suggestedForYou(value) {
+  const xpath = "//span[text() = 'Suggested for you']/ancestor::*[17]"
+  const div = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+  if (div != null && value) {
+    div.remove()
+    console.log("A 'Suggested for you' feature has been removed")
   }  
+}
+
+function stories(value) {
+  const div = document.querySelector('[data-pagelet="Stories"]')
+  if (div != null && value) {
+    div.remove()
+  }
+}
+
+function rooms(value) {
+  const xpath = "//span[text() = 'Create Room']/ancestor::*[15]"
+  const div = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+  if (div != null && value) {
+    div.remove()
+  }
 }
--- options.js
+++ options.js
@@ -1,18 +1,22 @@
 const saveButton = document.getElementById('save')
 
 function save_options() {
-  const pumkNews = document.getElementById('pumk1').checked;
-  const pumkProfile = document.getElementById('pumk2').checked;
-  const sugGroups = document.getElementById('sugGroups').checked;
+  const pumkNews = document.getElementById('pumk1').checked
+  const pumkProfile = document.getElementById('pumk2').checked
+  const sugGroups = document.getElementById('sugGroups').checked
+  const stories = document.getElementById('stories').checked
+  const rooms = document.getElementById('rooms').checked
   chrome.storage.sync.set({
     pumk1: pumkNews,
     pumk2: pumkProfile,
-    sugGroups: sugGroups
+    sugGroups: sugGroups,
+    stories: stories,
+    rooms: rooms
   }, function() {
-    const status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    const status = document.getElementById('status')
+    status.textContent = 'Options saved.'
     setTimeout(function() {
-      status.textContent = '';
+      status.textContent = ''
     }, 750);
   });
 }
@@ -21,13 +25,17 @@ function restore_options() {
   chrome.storage.sync.get({
     pumk1: true,
     pumk2: true,
-    sugGroups: true
+    sugGroups: true,
+    stories: true,
+    rooms: true
 }, function(items) {
-    document.getElementById('pumk1').checked = items.pumk1;
-    document.getElementById('pumk2').checked = items.pumk2;
+    document.getElementById('pumk1').checked = items.pumk1
+    document.getElementById('pumk2').checked = items.pumk2
     document.getElementById('sugGroups').checked = items.sugGroups
+    document.getElementById('stories').checked = items.stories
+    document.getElementById('rooms').checked = items.rooms
   });
 }
   
-document.addEventListener('DOMContentLoaded', restore_options);
-saveButton.addEventListener('click', save_options);
+document.addEventListener('DOMContentLoaded', restore_options)
+saveButton.addEventListener('click', save_options)
